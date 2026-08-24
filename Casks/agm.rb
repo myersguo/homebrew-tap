d440ab917af0e928ed1cc9f40c26b5818e4d2154
liangyong.guo <liangyong.guo@bytedance.com>
2026-08-24 14:50:40 +0800

    agm 0.9.0

--- Casks/agm.rb
+++ Casks/agm.rb
@@ -1,6 +1,6 @@
 cask "agm" do
-  version "0.8.0"
-  sha256 "76d7366b97f26f18487b02424fe7cb64ea2cd5e66b2c42db52fb494a5e1d7d46"
+  version "0.9.0"
+  sha256 "a1555c798f227fd209da339e408d516f1cdc112e94ae82ac2e5e5e8c0ec44819"
 
   url "https://github.com/myersguo/agm/releases/download/v#{version}/AGM-#{version}-macos-arm64-unsigned.zip"
   name "AGM"
@@ -8,17 +8,18 @@ cask "agm" do
   homepage "https://github.com/myersguo/agm"
 
   depends_on arch: :arm64
+  depends_on :macos
 
   app "AGM.app"
-
-  caveats <<~EOS
-    AGM #{version} is not signed or notarized yet. If macOS blocks the
-    first launch, clear the quarantine attribute for this app only:
-      xattr -dr com.apple.quarantine "#{appdir}/AGM.app"
-  EOS
 
   zap trash: [
     "~/Library/Application Support/com.myersguo.agm",
     "~/Library/WebKit/com.myersguo.agm",
   ]
+
+  caveats <<~EOS
+    AGM #{version} is ad-hoc signed, but not Developer ID signed or notarized.
+    If macOS blocks the first launch, clear the quarantine attribute for this app only:
+      xattr -dr com.apple.quarantine "#{appdir}/AGM.app"
+  EOS
 end
